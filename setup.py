0283de474195bf26bb884b52b97bf1af0e6bf2db
Mr_Hackin <jai005servanbhava@gmail.com>
2025-07-09 22:23:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="PyBharat",
+    name="pybharat",
     version="0.0.1",
     author="Jai Servana Bhava",
     author_email="jai005servanbhava@gmail.com",
